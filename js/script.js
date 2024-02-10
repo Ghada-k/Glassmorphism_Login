f12f633da9b619ee96ed7354929480e313fb6e01
Ghada <stare.ghada@gmail.com>
2024-02-10 14:48:18 +0100

    Js code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let signup = document.querySelector('#signup');
+let signin = document.querySelector('#signin');
 let body = document.querySelector('body');
 signup.onclick = function(){
     body.classList.add('signup');//to change the styling of the webpage when the user clicks the "signup" button.
+}
+signin.onclick = function(){
+    body.classList.remove('signup');//to change the styling of the webpage when the user clicks the "signup" button.
 }
